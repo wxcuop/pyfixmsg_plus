ac696bb4220b97c4e2dfffbac65e60533fbb4766
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-23 20:54:48 -0500

    Update handle_logs.py

--- pyfixmsg_plus/handle_logs.py
+++ pyfixmsg_plus/handle_logs.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from fixcommon.errors import ErrorLevel
+from errors import ErrorLevel
 
 class HandleLogFilesEvents:
     def HLF_NotifyMsg(self, s, level):
