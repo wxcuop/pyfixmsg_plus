23e93df31c586b028b34dbb4563a91365b664e18
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-23 21:11:26 -0500

    Create dbtools.py

---
+++ pyfixmsg_plus/dbtools.py
@@ -0,0 +1,15 @@
+class DBTools:
+    def __init__(self, a):
+        pass
+
+    def get_enrich_map_table(self, a, b):
+        pass
+
+    def get_stock_mapping_table(self, a, b, c):
+        pass
+
+    def store_for_fix(self, table_sent, seq, val):
+        pass
+
+    def store_stock_mapping_table(self, mapping):
+        pass
