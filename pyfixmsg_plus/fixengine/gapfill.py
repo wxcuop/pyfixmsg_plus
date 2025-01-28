1cf768ecba181ed1b99be905552317263b44a010
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-28 17:12:24 -0500

    Update gapfill.py

--- pyfixmsg_plus/fixengine/gapfill.py
+++ pyfixmsg_plus/fixengine/gapfill.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from pyfixmsg.fixmessage import FixMessage
 
-def send_gapfill(send_message, target_comp_id, seq_num, new_seq_num):
+def send_gapfill(send_message, config_manager, target_comp_id, seq_num, new_seq_num):
     message = FixMessage()
     message.update({
-        8: 'FIX.4.4',
+        8: config_manager.get('FIX', 'version', 'FIX.4.4'),
         35: '4',  # Sequence Reset (Gap Fill)
-        49: 'SERVER',
+        49: config_manager.get('FIX', 'sender', 'SERVER'),
         56: target_comp_id,
         34: seq_num,
         36: new_seq_num,  # New sequence number
