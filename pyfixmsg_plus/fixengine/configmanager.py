d06fc462ee76435dd19d9a918262873fb649146f
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-29 17:48:18 -0500

    Update configmanager.py

--- pyfixmsg_plus/fixengine/configmanager.py
+++ pyfixmsg_plus/fixengine/configmanager.py
@@ -20,10 +20,24 @@ class ConfigManager:
             self.config.add_section(section)
         self.config.set(section, option, value)
 
+    def delete(self, section, option=None):
+        if option:
+            self.config.remove_option(section, option)
+        else:
+            self.config.remove_section(section)
+
+    def reset(self):
+        self.config = configparser.ConfigParser()
+        self.save_config()
+
 # Example usage
 if __name__ == "__main__":
     cm = ConfigManager()
     cm.load_config()
     print(cm.get('FIX', 'sender_comp_id', 'SENDER'))
     cm.set('FIX', 'new_option', 'new_value')
+    cm.save_config()
+    cm.delete('FIX', 'new_option')
+    cm.save_config()
+    cm.reset()
     cm.save_config()
