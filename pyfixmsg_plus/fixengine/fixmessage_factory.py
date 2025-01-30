ad94a3ca54fc911ce65a78d3cbbb4b4b3aad3953
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-30 18:39:39 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -3,25 +3,11 @@ from fixmessage_builder import FixMessageBuilder
 class FixMessageFactory:
     @staticmethod
     def create_message(message_type, **kwargs):
-        builder = FixMessageBuilder().set_msg_type(message_type).set_fields(**kwargs)
-        if message_type == 'D':
-            return builder.build()
-        elif message_type == 'F':
-            return builder.build()
-        elif message_type == 'G':
-            return builder.build()
-        elif message_type == 'A':
-            return builder.build()
-        elif message_type == 'AB':
-            return builder.build()
-        elif message_type == 'AC':
-            return builder.build()
-        elif message_type == '8':
-            return builder.build()
-        elif message_type == '9':
-            return builder.build()
-        else:
-            raise ValueError("Unknown message type")
+        builder = FixMessageBuilder().set_msg_type(message_type)
+        for tag, value in kwargs.items():
+            builder.set_custom_field(tag, value)
+        
+        return builder.build()
 
 # Usage example
 # new_order_message = FixMessageFactory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
