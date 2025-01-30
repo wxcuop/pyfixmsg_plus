2e238951ae53332edf90d8a885a8fcab494858f7
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-30 18:34:37 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -5,9 +5,20 @@ class FixMessageFactory:
     def create_message(message_type, **kwargs):
         if message_type == 'D':
             return FixMessageBuilder().set_msg_type('D').set_fields(**kwargs).build()
+        elif message_type == 'F':
+            return FixMessageBuilder().set_msg_type('F').set_fields(**kwargs).build()
+        elif message_type == 'G':
+            return FixMessageBuilder().set_msg_type('G').set_fields(**kwargs).build()
+        elif message_type == 'A':
+            return FixMessageBuilder().set_msg_type('A').set_fields(**kwargs).build()
+        elif message_type == 'AB':
+            return FixMessageBuilder().set_msg_type('AB').set_fields(**kwargs).build()
+        elif message_type == 'AC':
+            return FixMessageBuilder().set_msg_type('AC').set_fields(**kwargs).build()
         elif message_type == '8':
             return FixMessageBuilder().set_msg_type('8').set_fields(**kwargs).build()
-        # Add other message types as needed
+        elif message_type == '9':
+            return FixMessageBuilder().set_msg_type('9').set_fields(**kwargs).build()
         else:
             raise ValueError("Unknown message type")
 
