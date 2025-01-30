3bba46a3f21080bf9f464a83cef1799cd3e04b36
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-30 18:37:04 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -3,22 +3,23 @@ from fixmessage_builder import FixMessageBuilder
 class FixMessageFactory:
     @staticmethod
     def create_message(message_type, **kwargs):
+        builder = FixMessageBuilder().set_msg_type(message_type).set_fields(**kwargs)
         if message_type == 'D':
-            return FixMessageBuilder().set_msg_type('D').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == 'F':
-            return FixMessageBuilder().set_msg_type('F').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == 'G':
-            return FixMessageBuilder().set_msg_type('G').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == 'A':
-            return FixMessageBuilder().set_msg_type('A').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == 'AB':
-            return FixMessageBuilder().set_msg_type('AB').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == 'AC':
-            return FixMessageBuilder().set_msg_type('AC').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == '8':
-            return FixMessageBuilder().set_msg_type('8').set_fields(**kwargs).build()
+            return builder.build()
         elif message_type == '9':
-            return FixMessageBuilder().set_msg_type('9').set_fields(**kwargs).build()
+            return builder.build()
         else:
             raise ValueError("Unknown message type")
 
