78dec4fc3fdb3db28578df9ea8c9173d18ef35a6
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-31 14:27:54 -0500

    Update message_store_factory.py

--- pyfixmsg_plus/fixengine/message_store_factory.py
+++ pyfixmsg_plus/fixengine/message_store_factory.py
@@ -12,5 +12,16 @@ class MessageStoreFactory:
 if __name__ == "__main__":
     db_path = 'fix_messages.db'
     store = MessageStoreFactory.get_message_store('database', db_path)
+    store.beginstring = 'FIX.4.4'
+    store.sendercompid = 'SENDER'
+    store.targetcompid = 'TARGET'
+    
     store.store_message('FIX.4.4', 'SENDER', 'TARGET', 1, 'Test message')
     print(store.get_message('FIX.4.4', 'SENDER', 'TARGET', 1))
+
+    print(store.get_next_outgoing_sequence_number())
+    print(store.get_next_incoming_sequence_number())
+    print("Reset sequence numbers to 1")
+    store.reset_sequence_numbers()
+    print(store.get_next_outgoing_sequence_number())
+    print(store.get_next_incoming_sequence_number())
