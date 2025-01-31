b7ffbf53dc7d4d78ff426fd92051529b0db083eb
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-31 14:17:36 -0500

    Update message_store_factory.py

--- pyfixmsg_plus/fixengine/message_store_factory.py
+++ pyfixmsg_plus/fixengine/message_store_factory.py
@@ -12,5 +12,5 @@ class MessageStoreFactory:
 if __name__ == "__main__":
     db_path = 'fix_messages.db'
     store = MessageStoreFactory.get_message_store('database', db_path)
-    store.store_message('FIX.4.4', 'SENDER', '', '', 'TARGET', '', '', '', 1, 'Test message')
-    print(store.get_message('FIX.4.4', 'SENDER', '', '', 'TARGET', '', '', '', 1))
+    store.store_message('FIX.4.4', 'SENDER', 'TARGET', 1, 'Test message')
+    print(store.get_message('FIX.4.4', 'SENDER', 'TARGET', 1))
