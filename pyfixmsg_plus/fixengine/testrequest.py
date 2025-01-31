d2d607c689f638752a66e032257c8a335a414913
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-30 21:52:57 -0500

    Update testrequest.py

--- pyfixmsg_plus/fixengine/testrequest.py
+++ pyfixmsg_plus/fixengine/testrequest.py
@@ -1,15 +1,14 @@
 from datetime import datetime
-from pyfixmsg.fixmessage import FixMessage
+from fixmessage_factory import FixMessageFactory
 
 def send_test_request(send_message, config_manager, seq_num):
-    message = FixMessage()
-    message.update({
-        8: config_manager.get('FIX', 'version', 'FIX.4.4'),
-        35: '1',  # Test Request
-        49: config_manager.get('FIX', 'sender', 'SERVER'),
-        56: config_manager.get('FIX', 'target', 'CLIENT'), 
-        34: seq_num,
-        52: datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f")[:-3],
-        112: 'TestReqID'  # Test Request ID
-    })
+    message = FixMessageFactory.create_message(
+        '1',
+        version=config_manager.get('FIX', 'version', 'FIX.4.4'),
+        sender=config_manager.get('FIX', 'sender', 'SERVER'),
+        target=config_manager.get('FIX', 'target', 'CLIENT'),
+        seq_num=seq_num,
+        test_req_id='TestReqID'
+    )
+    FixMessageFactory.return_message(message)
     send_message(message)
