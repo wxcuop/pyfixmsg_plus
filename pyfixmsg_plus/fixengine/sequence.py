13b707d9d1ef823a7c21e1e6dd9be8dd23f11f6e
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-31 14:00:37 -0500

    Update sequence.py

--- pyfixmsg_plus/fixengine/sequence.py
+++ pyfixmsg_plus/fixengine/sequence.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 class SequenceManager:
     def __init__(self, db_path):
@@ -9,10 +10,7 @@ class SequenceManager:
     def create_table(self):
         cursor = self.conn.cursor()
         cursor.execute('''
-            DROP TABLE IF EXISTS sessions
-        ''')
-        cursor.execute('''
-            CREATE TABLE sessions (
+            CREATE TABLE IF NOT EXISTS sessions (
                 beginstring TEXT NOT NULL,
                 sendercompid TEXT NOT NULL,
                 sendersubid TEXT NOT NULL,
@@ -33,16 +31,27 @@ class SequenceManager:
     def load_sequence_number(self):
         cursor = self.conn.cursor()
         cursor.execute('''
-            SELECT sequence_number FROM sequence WHERE id = 1
-        ''')
+            SELECT outgoing_seqnum FROM sessions WHERE
+            beginstring = ? AND sendercompid = ? AND sendersubid = ? AND senderlocid = ? AND
+            targetcompid = ? AND targetsubid = ? AND targetlocid = ? AND session_qualifier = ?
+        ''', (self.beginstring, self.sendercompid, self.sendersubid, self.senderlocid, 
+              self.targetcompid, self.targetsubid, self.targetlocid, self.session_qualifier))
         result = cursor.fetchone()
         return result[0] if result else 0
 
     def save_sequence_number(self, number):
         cursor = self.conn.cursor()
         cursor.execute('''
-            UPDATE sequence SET sequence_number = ? WHERE id = 1
-        ''', (number,))
+            INSERT INTO sessions (beginstring, sendercompid, sendersubid, senderlocid, 
+                                  targetcompid, targetsubid, targetlocid, session_qualifier, 
+                                  creation_time, incoming_seqnum, outgoing_seqnum)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT(beginstring, sendercompid, sendersubid, senderlocid, targetcompid, 
+                        targetsubid, targetlocid, session_qualifier)
+            DO UPDATE SET outgoing_seqnum = ?
+        ''', (self.beginstring, self.sendercompid, self.sendersubid, self.senderlocid, 
+              self.targetcompid, self.targetsubid, self.targetlocid, self.session_qualifier, 
+              datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), 0, number, number))
         self.conn.commit()
 
     def get_next_sequence_number(self):
@@ -54,5 +63,13 @@ class SequenceManager:
 if __name__ == "__main__":
     db_path = 'fix_state.db'
     sequence_manager = SequenceManager(db_path)
+    sequence_manager.beginstring = 'FIX.4.4'
+    sequence_manager.sendercompid = 'SENDER'
+    sequence_manager.sendersubid = ''
+    sequence_manager.senderlocid = ''
+    sequence_manager.targetcompid = 'TARGET'
+    sequence_manager.targetsubid = ''
+    sequence_manager.targetlocid = ''
+    sequence_manager.session_qualifier = ''
     print(sequence_manager.get_next_sequence_number())
     print(sequence_manager.get_next_sequence_number())
