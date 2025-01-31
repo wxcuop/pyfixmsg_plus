9352eb6d4c38865869655946dbe4a7906c847e6e
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-01-30 21:46:34 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -1,13 +1,23 @@
 from fixmessage_builder import FixMessageBuilder
+from fixmessage_pool import FixMessagePool
 
 class FixMessageFactory:
+    pool = FixMessagePool(size=20)  # Adjust pool size based on expected load
+
     @staticmethod
     def create_message(message_type, **kwargs):
-        builder = FixMessageBuilder().set_msg_type(message_type)
+        message = FixMessageFactory.pool.get_message()
+        builder = FixMessageBuilder(message).set_msg_type(message_type)
         for tag, value in kwargs.items():
             builder.set_custom_field(tag, value)
         
         return builder.build()
 
+    @staticmethod
+    def return_message(message):
+        FixMessageFactory.pool.return_message(message)
+
 # Usage example
-# new_order_message = FixMessageFactory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
+# factory = FixMessageFactory()
+# new_order_message = factory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
+# factory.return_message(new_order_message)
