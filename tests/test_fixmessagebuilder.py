2b7b3fc82dc5599287d9c8c89edeef3ccec7ef09
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-02 15:25:20 -0500

    Update test_fixmessagebuilder.py

--- tests/test_fixmessagebuilder.py
+++ tests/test_fixmessagebuilder.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 import pytest
-from .fixmessage_builder import FixMessageBuilder
+from pyfixmsg_plus.fixengine.fixmessage_builder import FixMessageBuilder
 
 def test_fixmessagebuilder_set_version():
     builder = FixMessageBuilder()
