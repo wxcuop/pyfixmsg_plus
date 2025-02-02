e26d68b337b88198e8cdf933a797c10dc8fb7d2f
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-01 23:03:10 -0500

    Create test_fixmessagefactory.py

---
+++ tests/test_fixmessagefactory.py
@@ -0,0 +1,14 @@
+import pytest
+from pyfixmsg_plus.fixengine.fixmessage_factory import FixMessageFactory
+
+def test_fixmessagefactory_create_message():
+    message = FixMessageFactory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
+    assert message[35] == 'D'
+    assert message[49] == 'SENDER'
+    assert message[56] == 'TARGET'
+    assert message['clordid'] == '12345'
+
+def test_fixmessagefactory_return_message():
+    message = FixMessageFactory.create_message('D')
+    FixMessageFactory.return_message(message)
+    # Since the pool is internal, we can't assert directly, but we can ensure no exceptions are raised
