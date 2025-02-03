8162b123a3ab00c4df1b78c68522ac43f63b57d8
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-02 20:24:18 -0500

    Update test_fixmessagefactory.py

--- tests/test_fixmessagefactory.py
+++ tests/test_fixmessagefactory.py
@@ -1,23 +1,9 @@
-import pickle
-from timeit import timeit
-import datetime
-import time
-import sys
-import os
-
-import six
 import pytest
-
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-
-
 from pyfixmsg_plus.fixengine.fixmessage_factory import FixMessageFactory
 from pyfixmsg.reference import FixSpec
 from pyfixmsg.codecs.stringfix import Codec
 
-
 SPEC = None
-
 
 @pytest.fixture
 def spec(request):
@@ -25,23 +11,16 @@ def spec(request):
     if SPEC is None:
         fname = request.config.getoption("--spec")
         if fname is None:
-            print("""
-
-      This test script needs to be invoked with the --spec
-      argument, set to the path to the FIX50.xml file from quickfix.org
-
-      """)
+            raise ValueError("This test script needs to be invoked with the --spec argument, set to the path to the FIX50.xml file from quickfix.org")
         SPEC = FixSpec(xml_file=fname)
     return SPEC
-
 
 @pytest.fixture
 def codec(spec):
     return Codec(spec=spec)
 
-
 def test_fixmessagefactory_create_message(codec):
-    FixMessageFactory.codec = codec  # Set the codec with spec
+    FixMessageFactory.set_codec(codec)  # Set the codec
     message = FixMessageFactory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
     print(message)
     assert message[35] == 'D'
@@ -49,9 +28,8 @@ def test_fixmessagefactory_create_message(codec):
     assert message[56] == 'TARGET'
     assert message['clordid'] == '12345'
 
-
 def test_fixmessagefactory_return_message(codec):
-    FixMessageFactory.codec = codec  # Set the codec with spec
+    FixMessageFactory.set_codec(codec)  # Set the codec
     message = FixMessageFactory.create_message('D')
     FixMessageFactory.return_message(message)
     # Since the pool is internal, we can't assert directly, but we can ensure no exceptions are raised
