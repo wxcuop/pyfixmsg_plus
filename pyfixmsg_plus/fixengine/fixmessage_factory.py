2b09fb135ed5c684875be3e05cc8a6cf411dce14
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-02 20:19:07 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -2,17 +2,18 @@ from .fixmessage_builder import FixMessageBuilder
 from .fixmessage_pool import FixMessagePool
 
 class FixMessageFactory:
-    pool = FixMessagePool(size=20)  # Adjust pool size based on expected load
+    pool = None
     codec = None  # Add a codec attribute
 
     @staticmethod
     def set_codec(codec):
-        FixMessageFactory.codec = codec  # Method to set the codec
+        FixMessageFactory.codec = codec
+        FixMessageFactory.pool = FixMessagePool(size=20, codec=codec)  # Initialize pool with codec
 
     @staticmethod
     def create_message(message_type, **kwargs):
         message = FixMessageFactory.pool.get_message()
-        builder = FixMessageBuilder(message, codec=FixMessageFactory.codec).set_msg_type(message_type)  # Pass the codec to the builder
+        builder = FixMessageBuilder(message, codec=FixMessageFactory.codec).set_msg_type(message_type)
         for tag, value in kwargs.items():
             builder.set_custom_field(tag, value)
         
@@ -21,9 +22,3 @@ class FixMessageFactory:
     @staticmethod
     def return_message(message):
         FixMessageFactory.pool.return_message(message)
-
-# Usage example
-# factory = FixMessageFactory()
-# factory.set_codec(your_codec)
-# new_order_message = factory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
-# factory.return_message(new_order_message)
