13530a7e159958c127c7b661f9ae55661705797d
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-02 20:12:03 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -3,11 +3,16 @@ from .fixmessage_pool import FixMessagePool
 
 class FixMessageFactory:
     pool = FixMessagePool(size=20)  # Adjust pool size based on expected load
+    codec = None  # Add a codec attribute
+
+    @staticmethod
+    def set_codec(codec):
+        FixMessageFactory.codec = codec  # Method to set the codec
 
     @staticmethod
     def create_message(message_type, **kwargs):
         message = FixMessageFactory.pool.get_message()
-        builder = FixMessageBuilder(message).set_msg_type(message_type)
+        builder = FixMessageBuilder(message, codec=FixMessageFactory.codec).set_msg_type(message_type)  # Pass the codec to the builder
         for tag, value in kwargs.items():
             builder.set_custom_field(tag, value)
         
@@ -19,5 +24,6 @@ class FixMessageFactory:
 
 # Usage example
 # factory = FixMessageFactory()
+# factory.set_codec(your_codec)
 # new_order_message = factory.create_message('D', sender='SENDER', target='TARGET', clordid='12345')
 # factory.return_message(new_order_message)
