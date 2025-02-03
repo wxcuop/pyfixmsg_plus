c48996b6bb4708c378c8d447f707d263b7442938
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-02 20:57:28 -0500

    Update fixmessage_factory.py

--- pyfixmsg_plus/fixengine/fixmessage_factory.py
+++ pyfixmsg_plus/fixengine/fixmessage_factory.py
@@ -4,18 +4,20 @@ from .fixmessage_pool import FixMessagePool
 class FixMessageFactory:
     pool = None
     codec = None  # Add a codec attribute
+    fragment_class = None  # Add a fragment_class attribute
 
     @staticmethod
-    def set_codec(codec):
+    def set_codec(codec, fragment_class=None):
         FixMessageFactory.codec = codec
-        FixMessageFactory.pool = FixMessagePool(size=20, codec=codec)  # Initialize pool with codec
+        FixMessageFactory.fragment_class = fragment_class
+        FixMessageFactory.pool = FixMessagePool(size=20, codec=codec, fragment_class=fragment_class)  # Initialize pool with codec and fragment_class
 
     @staticmethod
     def create_message(message_type, **kwargs):
         if FixMessageFactory.pool is None:
             raise ValueError("FixMessageFactory.pool is not initialized. Call set_codec first.")
         message = FixMessageFactory.pool.get_message()
-        builder = FixMessageBuilder(message, codec=FixMessageFactory.codec).set_msg_type(message_type)
+        builder = FixMessageBuilder(message, codec=FixMessageFactory.codec, fragment_class=FixMessageFactory.fragment_class).set_msg_type(message_type)
         for tag, value in kwargs.items():
             builder.set_custom_field(tag, value)
         
