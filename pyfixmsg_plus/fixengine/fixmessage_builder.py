90c422ee27a068d9323ef4e3f807f1b3c268b3ae
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-02-03 22:39:45 -0500

    Update fixmessage_builder.py

--- pyfixmsg_plus/fixengine/fixmessage_builder.py
+++ pyfixmsg_plus/fixengine/fixmessage_builder.py
@@ -15,19 +15,19 @@ class FixMessageBuilder:
 
     def set_version(self, version):
         return self.set_fixtag_by_name('BeginString', version)
-
+    
     def set_msg_type(self, msg_type):
         return self.set_fixtag_by_name('MsgType', msg_type)
-
+    
     def set_sender(self, sender):
         return self.set_fixtag_by_name('SenderCompID', sender)
-
+    
     def set_target(self, target):
         return self.set_fixtag_by_name('TargetCompID', target)
-
+    
     def set_sequence_number(self, seq_number):
         return self.set_fixtag_by_name('MsgSeqNum', seq_number)
-
+    
     def set_sending_time(self):
         sending_time = datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]
         return self.set_fixtag_by_name('SendingTime', sending_time)
@@ -35,7 +35,7 @@ class FixMessageBuilder:
     def set_fixtag(self, tag, value):
         self.message[tag] = value
         return self
-
+    
     def set_fixtag_by_name(self, tag_name, value):
         tag_number = self.fix_spec.tags.get(tag_name)
         if tag_number is not None:
@@ -44,18 +44,26 @@ class FixMessageBuilder:
             raise ValueError(f"Tag name '{tag_name}' not found in FixSpec.")
         return self
 
+    def set_direction(self, direction):
+        self.message.direction = direction
+        return self
+
+    def set_recipient(self, recipient):
+        self.message.recipient = recipient
+        return self
+
     def build(self):
         if self.codec:
             return self.codec.serialise(self.message)
         return self.message
-
+    
     def get_message(self):
         return self.message
-
+    
     def update_message(self, tags_dict):
         self.message.update(tags_dict)
         return self
-
+    
     def reset_message(self):
         self.message = FixMessage(codec=self.codec)
         return self
