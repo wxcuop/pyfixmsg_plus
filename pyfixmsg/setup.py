ba8484b3ab4b1575a839d27c0306cea25c04d967
wxcuop <94150159+wxcuop@users.noreply.github.com>
2025-03-21 22:14:00 -0400

    Create setup.py

---
+++ pyfixmsg/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfixmsg",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "six>=1.12.0",
+        # Add other dependencies here
+    ],
+)
